80dff73f0d028cab855fb19beab859b71dfa27f5
Brian Seel <brian.seel@gmail.com>
2025-08-24 14:31:05 -0400

    Remove infrastructure code

--- infra/terraform/providers.tf
+++
@@ -1,9 +0,0 @@
-variable "cloudflare_api_token" {
-  type      = string
-  sensitive = true
-  description = "Cloudflare API token with least-privilege (Workers:Edit, D1:Edit)"
-}
-
-provider "cloudflare" {
-  api_token = var.cloudflare_api_token
-}
